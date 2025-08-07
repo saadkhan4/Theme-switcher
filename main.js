0bc01d28973d320ea4aeedc44257195a5a6c358b
saadkhan4 <saadshaheen1327@gmail.com>
2025-08-07 22:36:36 +0500

    JS file created

---
+++ main.js
@@ -0,0 +1,29 @@
+function updateToggleUI(theme) {
+    if (theme === "dark") {
+        toggleIcon.textContent = "☀️";
+        toggleText.textContent = "Light Mode";
+    } else {
+        toggleIcon.textContent = "🌙";
+        toggleText.textContent = "Dark Mode"
+    }
+}
+
+// functionality for theme toggle
+const themeToggle = document.getElementById("theme-toggle");
+const toggleIcon = themeToggle.querySelector(".toggle-icon");
+const toggleText = themeToggle.querySelector(".toggle-text");
+
+// Get saved theme or default to light
+// We set the key "theme" in localstorage of browser
+const currentTheme = localStorage.getItem("theme") || "light";
+document.documentElement.setAttribute("data-theme", currentTheme);
+updateToggleUI(currentTheme);
+
+themeToggle.addEventListener("click", () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+    updateToggleUI(newTheme);
+})
